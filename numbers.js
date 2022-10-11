d67bc52b12b34b4bb886967cb52911c288e53013
Camila Guedes <camila.guedes1977@gmail.com>
2022-10-11 11:53:22 -0300

    Exemplos da aula 1

---
+++ numbers.js
@@ -0,0 +1,30 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+
+// NaN not a number
+
+const naoNumero = "1";
+
+const idade = 28
+const pi = 3.14
+
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
+
+var a = 0
+var b = 0
+console.log(a/b) // NaN
---
+++ tipostring.js
@@ -0,0 +1,25 @@
+const texto1 = "Olá Mundo!";
+const texto2 = 'Olá Mundo!';
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
